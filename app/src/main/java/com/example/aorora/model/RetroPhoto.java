aa4fe9e74d0fabf8c9ec81517045570239cca0a7
Carli Martinez <martinezcarli@gmail.com>
2019-04-11 11:24:33 -0700

    Update RetroPhoto.java

--- app/src/main/java/com/example/aorora/model/RetroPhoto.java
+++ app/src/main/java/com/example/aorora/model/RetroPhoto.java
@@ -4,62 +4,40 @@ import com.google.gson.annotations.SerializedName;
 
 public class RetroPhoto {
 
-    @SerializedName("albumId")
-    private Integer albumId;
-    @SerializedName("id")
-    private Integer id;
-    @SerializedName("title")
-    private String title;
-    @SerializedName("url")
-    private String url;
-    @SerializedName("thumbnailUrl")
-    private String thumbnailUrl;
+    @SerializedName("butterfly_id")
+    private Integer butterfly_id;
+    @SerializedName("butterfly_type_id")
+    private Integer butterfly_type_id;
+    @SerializedName("butterfly_create_at")
+    private String butterfly_create_at;
 
-    public RetroPhoto(Integer albumId, Integer id, String title, String url, String thumbnailUrl) {
-        this.albumId = albumId;
-        this.id = id;
-        this.title = title;
-        this.url = url;
-        this.thumbnailUrl = thumbnailUrl;
+    public RetroPhoto(Integer butterfly_id, Integer butterfly_type_id, String butterfly_create_at) {
+        this.butterfly_id = butterfly_id;
+        this.butterfly_type_id = butterfly_type_id;
+        this.butterfly_create_at = butterfly_create_at;
     }
 
-    public Integer getAlbumId() {
-        return albumId;
+    public Integer getButterflyId() {
+        return butterfly_id;
     }
 
-    public void setAlbumId(Integer albumId) {
-        this.albumId = albumId;
+    public void setButterflyId(Integer butterfly_id) {
+        this.butterfly_id = butterfly_id;
     }
 
-    public Integer getId() {
-        return id;
+    public Integer getButterflyTypeId() {
+        return butterfly_type_id;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public void setButterflyTypeId(Integer butterfly_type_id) {
+        this.butterfly_type_id = butterfly_type_id;
     }
 
-    public String getTitle() {
-        return title;
+    public Integer getButterflyCreatedAt() {
+        return butterfly_create_at;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getThumbnailUrl() {
-        return thumbnailUrl;
-    }
-
-    public void setThumbnailUrl(String thumbnailUrl) {
-        this.thumbnailUrl = thumbnailUrl;
+    public void getButterflyCreatedAt(Integer butterfly_create_at) {
+        this.butterfly_create_at = butterfly_create_at;
     }
 }
