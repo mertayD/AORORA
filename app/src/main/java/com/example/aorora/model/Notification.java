a6af96c09feb6c8685f863980c367efeabbefb66
Keller <kjmikkelson05@gmail.com>
2019-12-04 15:42:08 -0700

    Addition of Notification to model and accounting for that
    The Notification model needed to be added to the phone as it is on the backend of the server. I also started changing the parts where UserInteraction was used incorrectly into Notification. Mainly the collection of the necessary notifications. I will might use UserInteraction to find all the notifications required, or add the receiver and initiator to Notification.

---
+++ app/src/main/java/com/example/aorora/model/Notification.java
@@ -0,0 +1,53 @@
+package com.example.aorora.model;
+
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.annotations.Expose;
+
+public class Notification
+{
+
+    @SerializedName("user_notification_id")
+    private Integer user_notification_id;
+    @SerializedName("user_notification_type_id")
+    private Integer user_notification_type_id;
+    @SerializedName("notification_user_id")
+    private Integer notification_user_id;
+    @SerializedName("user_interaction_created_at")
+    private String user_interaction_created_at;
+
+    public Notification(Integer user_interaction_id, Integer user_interaction_type_id,
+                           Integer initiator_user_id, Integer receiver_user_id, Integer quest_record_id,
+                           String user_interaction_content, String user_interaction_created_at)
+    {
+        this.user_notification_id = user_interaction_id;
+        this.user_notification_type_id = user_interaction_type_id;
+        this.notification_user_id = initiator_user_id;
+        this.user_interaction_created_at = user_interaction_created_at;
+    }
+
+    public Integer getNotification_id() {return user_notification_id;}
+    public Integer getNotification_type_id() {return user_notification_type_id;}
+    public Integer getNotification_user_id() {return notification_user_id;}
+    public String getNotification_created_at() {return user_interaction_created_at;}
+
+    public void setNotification_id(Integer user_interaction_id)
+    {
+        this.user_notification_id = user_interaction_id;
+    }
+
+    public void Notification_type_id(Integer user_interaction_type_id)
+    {
+        this.user_notification_type_id = user_interaction_type_id;
+    }
+
+    public void setNotification_user_id(Integer initiator_user_id)
+    {
+        this.notification_user_id = initiator_user_id;
+    }
+
+    public void setNotification_created_at(String user_interaction_created_at)
+    {
+        this.user_interaction_created_at = user_interaction_created_at;
+    }
+
+}
