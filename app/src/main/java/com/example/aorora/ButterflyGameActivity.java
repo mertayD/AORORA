905c0e55196817a42c27321027dfcead7beeee46
Dumilk <dtd66@nau.edu>
2021-04-01 01:58:28 -0700

    further cleaning of unused code

--- app/src/main/java/com/example/aorora/ButterflyGameActivity.java
+++ app/src/main/java/com/example/aorora/ButterflyGameActivity.java
@@ -1,15 +1,12 @@
 package com.example.aorora;
 
-import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.media.Image;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 
 import androidx.annotation.NonNull;
-import androidx.camera.core.AspectRatio;
 import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageAnalysis;
@@ -18,20 +15,13 @@ import androidx.camera.core.Preview;
 import androidx.camera.extensions.HdrImageCaptureExtender;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
-import androidx.constraintlayout.solver.widgets.ResolutionDimension;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.lifecycle.LifecycleOwner;
 
-import android.print.PrintAttributes;
-import android.util.DisplayMetrics;
-import android.util.Rational;
-import android.util.Size;
 import android.util.TypedValue;
 import android.view.View;
-import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -40,10 +30,8 @@ import com.example.aorora.butterflyGame.Basket;
 import com.example.aorora.butterflyGame.Butterfly;
 import com.example.aorora.butterflyGame.ButterflyBasketOnDragListener;
 import com.example.aorora.network.NetworkCalls;
-import com.google.ar.sceneform.rendering.CameraProvider;
 import com.google.common.util.concurrent.ListenableFuture;
 
-import java.text.SimpleDateFormat;
 import java.util.Random;
 import java.util.concurrent.ExecutionException;
 
@@ -61,7 +49,6 @@ public class ButterflyGameActivity extends AppCompatActivity {
     Context mContext;
 
     ConstraintLayout layout;
-    ConstraintLayout.LayoutParams params;
 
     /**
      *  default max time for in game time: set to 30 seconds
@@ -103,7 +90,7 @@ public class ButterflyGameActivity extends AppCompatActivity {
     private TextView gameTimeText;
 
     /**
-     * time left in miisceonds starting at default max time
+     * time left in milliseconds starting at default max time
      */
     private long timeLeftInMilliseconds = MAX_TIMER_MILLISECONDS;
 
@@ -114,11 +101,11 @@ public class ButterflyGameActivity extends AppCompatActivity {
 
         mContext = this;
 
-        layout = (ConstraintLayout) findViewById(R.id.layout);
-        basket = (Basket) findViewById(R.id.basket);
-        mPreviewView = (PreviewView) findViewById(R.id.previewView);
-        cameraSwitch = (Switch) findViewById(R.id.cameraSwitch);
-        gameTimeText = (TextView) findViewById(R.id.gameTimeText);
+        layout = findViewById(R.id.layout);
+        basket = findViewById(R.id.basket);
+        mPreviewView = findViewById(R.id.previewView);
+        cameraSwitch = findViewById(R.id.cameraSwitch);
+        gameTimeText = findViewById(R.id.gameTimeText);
 
         //initialize camera preview
         if (cameraPermissionsGranted()) {
@@ -137,7 +124,7 @@ public class ButterflyGameActivity extends AppCompatActivity {
         Random seed = new Random();
 
         //GAME LIFECYCLE
-        CountDownTimer gameTimer = new CountDownTimer(timeLeftInMilliseconds, 1000) {
+        new CountDownTimer(timeLeftInMilliseconds, 1000) {
             @Override
             public void onTick(long l) {
 
@@ -233,7 +220,7 @@ public class ButterflyGameActivity extends AppCompatActivity {
         // update user info table local
         MainActivity.user_info.update_local_atrium(basket.basketContents);
 
-        // update network db with hashmap
+        // update network db with hash map
         NetworkCalls.updateUserAtrium(MainActivity.user_info.getUser_id(),
                 basket.basketContents, getApplicationContext());
 
@@ -278,9 +265,6 @@ public class ButterflyGameActivity extends AppCompatActivity {
                 .requireLensFacing(CameraSelector.LENS_FACING_BACK)
                 .build();
 
-        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
-                .build();
-
         ImageCapture.Builder builder = new ImageCapture.Builder();
 
         //Vendor-Extensions (The CameraX extensions dependency in build.gradle)
@@ -292,21 +276,14 @@ public class ButterflyGameActivity extends AppCompatActivity {
             hdrImageCaptureExtender.enableExtension(cameraSelector);
         }
 
-        final ImageCapture imageCapture = builder
-                .setTargetRotation(this.getWindowManager().getDefaultDisplay().getRotation())
-                .build();
-
         preview.setSurfaceProvider(mPreviewView.getSurfaceProvider());
 
         Camera camera = cameraProvider.bindToLifecycle
                 (
-                    this,
-                    cameraSelector,
-                    preview,
-                    imageAnalysis,
-                    imageCapture
+                        this,
+                        cameraSelector,
+                        preview
                 );
-
     }
 
     /**
@@ -356,15 +333,6 @@ public class ButterflyGameActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
-        );
-    }
-
-    private void showSystemUI() {
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         );
     }
 }
